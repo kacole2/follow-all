247dace4f4b70f274bb3b0c1a7b2d214b95fca43
Kendrick Coleman <kendrickcoleman@gmail.com>
2016-06-07 15:55:46 -0400

    fixed space vs tab
    really? i swear this is cranky
    
    Signed-off-by: Kendrick Coleman <kendrickcoleman@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,10 @@ class User
 
   def follow_list(user, list)
     users_to_follow = @client.list_members(user, list, skip_status: true, include_entities: false).to_a
-   	clientId = @client.user.id
-   	users_to_follow.each do |user|
-   		p user.id
-   		if clientId == user.id
-   			users_to_follow.delete(user)
-   		end
-   	end
+    client_id = @client.user.id
+    users_to_follow.each do |new_user|
+      users_to_follow.delete(new_user) if client_id == new_user.id
+    end
     @client.follow(users_to_follow)
   end
 end
